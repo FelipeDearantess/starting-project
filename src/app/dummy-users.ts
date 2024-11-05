46c6461a0db6cf8849c3045d2473739ec4bfd1d7
Felipe de Moura1~ <Felipearantess@gmail.com>
2024-11-05 16:27:44 -0300

    qualquer coisa

---
+++ src/app/dummy-users.ts
@@ -0,0 +1,31 @@
+export const DUMMY_USERS =[
+ { id: 'u1',
+  name:"Emily Thompson",
+  avatar: "user-3.jpg"
+},
+{ id: 'u2',
+name:"marcus thompson",
+avatar: "user-2.jpg"
+},
+{ id: 'u3',
+name:"david Thompson",
+avatar: "user-3.jpg"
+},
+{ id: 'u4',
+name:"carlos Thompson",
+avatar: "user-4.jpg"
+},
+{ id: 'u5',
+name:"mario Thompson",
+avatar: "user-5.jpg"
+},
+{ id: 'u6',
+name:"luana Thompson",
+avatar: "user-6.jpg"
+},
+]
+
+
+
+
+
